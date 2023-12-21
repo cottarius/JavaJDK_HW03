67eb749aafd4ed4ba0ae5b69c891316ae45585fe
Oleg Prokopenko <cotarius@yandex.ru>
2023-12-21 19:50:38 +0300

    update JavaJDK_HW03

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,11 @@ public class Main {
     }
 
     private static void ex1() {
+        /**
+         * Написать класс Калькулятор (необобщенный), который содержит обобщенные статические методы: sum(),
+         * multiply(), divide(), subtract(). Параметры этих методов – два числа разного типа, над которыми должна
+         * быть произведена операция.
+         */
         System.out.println("Задание 1");
         MyCalculator.sum(5L, 5L);
         MyCalculator.multiply(5, 5.5);
@@ -19,6 +24,11 @@ public class Main {
     }
 
     private static void ex2() {
+        /**
+         * Напишите обобщенный метод compareArrays(), который принимает два массива и возвращает true, если они
+         * одинаковые, и false в противном случае. Массивы могут быть любого типа данных, но должны иметь одинаковую
+         * длину и содержать элементы одного типа по парно.
+         */
         System.out.println("Задание 2");
         List<String> listOfStrings = Stream.of("один", "два", "три", "четыре").toList();
 
@@ -55,6 +65,11 @@ public class Main {
         System.out.println();
     }
     private static void ex3(){
+        /**
+         * Напишите обобщенный класс Pair, который представляет собой пару значений разного типа. Класс должен иметь
+         * методы getFirst(), getSecond() для получения значений каждого из составляющих пары, а также переопределение
+         * метода toString(), возвращающее строковое представление пары. Работу сдать в виде ссылки на гит репозиторий
+         */
         System.out.println("Задание 3");
         Pair<Integer, String> pair = new Pair<>(1, "два");
         System.out.println(pair.getFirstValue());
